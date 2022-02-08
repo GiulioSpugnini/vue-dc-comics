c15b101615ed74e1ace52f494316847c06f7a061
GiulioSpugnini <giuliospugnini@yahoo.com>
2022-02-08 15:48:48 +0100

    added and implement Header.vue

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,25 @@
 <template>
-<div></div>
+<div>
+  <header>
+    <Header/>
+  </header>
+</div>
 </template>
 
 <script>
-
+import Header from './components/Header.vue';
 export default {
   name: 'App',
   components: {
+    Header,
   }
 }
 </script>
 
 <style lang="scss">
-
+*{
+  margin: 0 auto;
+  max-width: 1200px;
+  padding: 0;
+}
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,54 @@
+<template>
+  <div>
+      <figure>
+          <img src="../assets/dc-logo.png" alt="">
+      </figure>
+      <nav>
+          <ul>
+              <li v-for='link in links' :key='link.text'>{{link.text}}</li>
+          </ul>
+      </nav>
+  </div>
+</template>
+
+<script>
+export default {
+name: 'Header',
+data(){
+    return{
+        links:[
+            {text: 'CHARACTERS', url: '#', active:false},
+            {text: 'COMICS', url: '#', active:false},
+            {text: 'MOVIES', url: '#', active:false},
+            {text: 'TV', url: '#', active:false},
+            {text: 'GAMES', url: '#', active:false},
+            {text: 'COLLECTIBLES', url: '#', active:false},
+            {text: 'VIDEOS', url: '#', active:false},
+            {text: 'FANS', url: '#', active:false},
+            {text: 'NEWS', url: '#', active:false},
+            {text: 'SHOP', url: '#', active:false},
+        ],
+};
+
+},
+};
+</script>
+
+<style scoped>
+    div{
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        margin-top: 50px;
+    }
+    ul{
+        display: flex;
+        justify-content: space-between;
+    }
+    li{
+        list-style: none;
+        text-decoration: none;
+        padding: 10px;
+        font-weight: bolder;
+    }
+</style>
